21f10c36b3952a668856e1b553c22a03919e8914
Jesse Vincent <jesse@fsck.com>
2025-05-25 20:14:07 -0700

    Switch swift-sdk to load from cloud

--- MacMCP/Package.swift
+++ MacMCP/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     .executable(name: "mcp-ax-inspector", targets: ["MCPAccessibilityInspector"]),
   ],
   dependencies: [
-    .package(path: "../swift-sdk"),
+    .package(url: "https://github.com/modelcontextprotocol/swift-sdk", branch: "main"),
     .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
     .package(url: "https://github.com/apple/swift-log.git", from: "1.5.0"),
     .package(path: "Plugins/SwiftLintPlugin"),
